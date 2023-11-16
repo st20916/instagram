b59baa7015c1b16e54e0c10f9993223e0d6187d5
st20916 <83683752+st20916@users.noreply.github.com>
2023-11-16 20:24:17 +0900

    fix : 삭제 이벤트 추가, 게시 클릭 이벤트 후 아이콘 안 눌리던 오류 수정

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).ready(function() {
     var $container = $('[data-container]'),
         $modal = $('.modal');
     // Container 내 event 호출
@@ -12,11 +12,11 @@ $(function() {
                 // 최초 hasClass로 like 클래스 판별
                 if ($(this).hasClass('like')) {
                     // 있을 경우, 좋아요 상태
-                    $(this).css('background', 'url(../images/background03.png) no-repeat no-repeat -41px 0');
+                    $(this).css('background', "url(../images/background03.png) no-repeat -41px 0");
                     $(this).removeClass('like');
                 } else {
                     // 없을 경우, 좋아요 상태 X
-                    $(this).css('background', 'url(../images/background03.png) no-repeat no-repeat -6px 0');
+                    $(this).css('background', "url(../images/background03.png) no-repeat -6px 0");
                     $(this).addClass('like');
                 }
                 break;
@@ -38,6 +38,22 @@ $(function() {
                 break;
         }
     });
+    // localstorage 로드
+    var firstComments = getComment();
+    
+    firstComments.forEach(function (comment, idx) {
+        $('#' + comment.id).append(
+            '<div class="comment_container">'
+            +   '   <input type="hidden" name="comment" value="' + Number(idx + 1) + '">'
+            +   '   <div class="comment">'
+            +   '       <img src="/images/thumb02.jpg" alt="profile" />'
+            +   '       <div class="nick_name m_text">User</div>'
+            +   '       <div>' + comment.text + '</div>'
+            +   '       <a href="javascript:void(0);" id="cmt' + Number(idx + 1) + '" class="comment__modal" data-id="' + comment.id + '"></a>'
+            +   '   </div>'
+            +   '</div>'
+        )
+    });
     // 댓글 창에 onFocus 통해 게시 Show
     $('.comment_field > input[type="text"]').on('focus', function(e) {
         $(this).siblings().show();
@@ -48,50 +64,38 @@ $(function() {
         $val = $($(this).siblings()).val();
         
         // val 값으로 json 저장..
-        addComment($val, $commentId);
+        if ($val.length > 0) {
+            addComment($val, $commentId);
+        } else {
+            alert('댓글을 입력해 주세요.');
+        }
         comments = getComment();
         
         $('#' + $commentId).html("");
-    
+
         comments.forEach(function (comment, idx) {
             if (comment.id == $commentId) {
                 $('#' + comment.id).append(
                     '<div class="comment_container">'
                     +   '   <input type="hidden" name="comment" value="' + Number(idx + 1) + '">'
                     +   '   <div class="comment">'
-                    +   '       <img src="../images/thumb02.jpg" alt="profile" />'
+                    +   '       <img src="/images/thumb02.jpg" alt="profile" />'
                     +   '       <div class="nick_name m_text">User</div>'
                     +   '       <div>' + comment.text + '</div>'
-                    +   '       <a href="javascript:void(0);" class="comment__modal" data-id="' + comment.id + '"></a>'
+                    +   '       <a href="javascript:void(0);" id="cmt' + Number(idx + 1) + '" class="comment__modal" data-id="' + comment.id + '"></a>'
                     +   '   </div>'
                     +   '</div>'
                 )
             }
         });
-    
         $(this).siblings("input").val('');
         $(this).hide();
     });
-    // localstorage 로드
-    var firstComments = getComment();
-    
-    firstComments.forEach(function (comment, idx) {
-        $('#' + comment.id).append(
-            '<div class="comment_container">'
-            +   '   <input type="hidden" name="comment" value="' + Number(idx + 1) + '">'
-            +   '   <div class="comment">'
-            +   '       <img src="../images/thumb02.jpg" alt="profile" />'
-            +   '       <div class="nick_name m_text">User</div>'
-            +   '       <div>' + comment.text + '</div>'
-            +   '       <a href="javascript:void(0);" class="comment__modal" data-id="' + comment.id + '"></a>'
-            +   '   </div>'
-            +   '</div>'
-        )
-    });
     // 댓글 옆 아이콘 클릭 시, 모달 팝업 표출
-    $('.comment_container').find('.comment__modal').on('click', function(e) {
+    // 동적으로 생성 이후, 최초 한 번만 이벤트가 바인딩 되기 때문에 body에다가 이벤트 위임
+    $('body').on('click', '.comment__modal' ,function(e) {        
         var $this = $(this);
-
+       
         $modal.addClass('show');
         $modal.attr('data-id', $(this).data('id'));
         $modal.find('#cmmtDelete').attr('data-num', $this.closest('div').siblings('input').val());
@@ -101,10 +105,12 @@ $(function() {
         $(this).closest('.modal').removeClass('show');
         $(this).closest('.modal').removeAttr('data-id');
     });
-    // 삭제 버튼 클릭 시, 해당 data-num에 대한 localstorage 제거
+    // 삭제 버튼 클릭 시,
     $('#cmmtDelete').on('click', function() {
         var closestModal = $(this).closest('.modal'),
             id = closestModal.data('id') || '';
+
+        deleteComment($(this).data('num'));
     });
 
     /**
@@ -139,9 +145,8 @@ $(function() {
     function deleteComment(index) {
         var comments = getComment();
 
-        comments.splice(index - 1);
+        comments.splice(index - 1, 1);
         localStorage.setItem('comments', JSON.stringify(comments));
-
-        location.reload();
     }
 });
+
